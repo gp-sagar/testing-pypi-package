714fba1870f4a70f25130b75f54a63d36d103203
Sagar Singh <sagarsingh@polarisgrids.com>
2025-03-10 17:59:52 +0530

    add some func and update version

--- dlmsCommandRequestCsvs/main.py
+++ dlmsCommandRequestCsvs/main.py
@@ -1,2 +1,20 @@
-def maisagarhoo():
-    return "maisagarhoo, and aap ?"
+def greet():
+    return "welcome to polarisgrids ⚜️"
+
+def calculate(a, b, o):
+    if o == '+':
+        result = a + b
+    elif o == '-':
+        result = a - b
+    elif o == '*':
+        result = a * b
+    elif o == '/':
+        result = a / b
+    else:
+        return 'Invalid operation'
+    
+    return f'The Value is :: {result}'
+
+def printbysagar():
+    print('Hello')
+    return 'Hello ji'
